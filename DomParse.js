ea3342db28850e494d31f09c785adc265917d075
Max Markowitz <knightsbore@gmail.com>
2013-07-06 19:58:06 -0700

    initial commit
    Encode
    Decode
    NL2br
    Br2nl
    Removebytagname
    RemoveEmptyContainers

---
+++ DomParse.js
@@ -0,0 +1,59 @@
+function EncodeHTML(str){
+    return String(str).replace(/&/g, '&amp;')
+            		  .replace(/"/g, '&quot;')
+            		  .replace(/'/g, '&#39;')
+            		  .replace(/</g, '&lt;')
+            		  .replace(/>/g, '&gt;');
+};
+
+function DecodeHTML(str){
+	return String(str).replace(/&amp;/g, '&')
+					  .replace(/&quot;/g, '"')
+					  .replace(/&#39;/g, "'")
+					  .replace(/&lt;/g, '<')
+					  .replace(/&gt;/g, '>');
+};
+
+function Nl2br(str){
+	return String(str).replace(/<\r?\n/g, '</br>');
+};	
+
+function Br2nl(str){
+	return String(str).replace('</br>', '\n');
+};
+
+/*
+	All Dom function create a temp container and attach the html 
+	to be parsed to the innerhtml.
+*/
+function RemoveByTagName(html, tag){
+    var container = document.createElement('div');
+    container.id = 'tempContainer';
+    document.body.appendChild(container);
+    container.innerHTML = html;
+    var breaks = container.getElementsByTagName(tag);
+    for (var i = 0, var len = breaks.length; i < len; i++) {
+    	breaks[i].parentNode.removeChild(breaks[i]);
+        i--;
+	}
+    document.body.removeChild(container);
+    return String(container.innerHTML);
+};
+function RemoveEmptyContainers(html){
+    var container = document.createElement('div');
+    container.id = 'tempContainer';
+    document.body.appendChild(container);
+    container.innerHTML = html;
+    var nodes = container.getElementsByTagName('*');
+    for (var i = 0; var len = nodes.length; i < len; i++) {
+        // Check if image to avoid deleting images
+        // which often times have no innerhtml
+        if (nodes[i].tagName != 'IMG'){
+            if (nodes[i].innerHTML == ''){
+                nodes[i].parentNode.removeChild(nodes[i]);
+            }
+        }
+    }
+    document.body.removeChild(container);
+    return String(container.innerHTML);
+};
